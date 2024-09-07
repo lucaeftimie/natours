9e85249acfb4226340d8656f41e5de76d5193252
lucaeftimie <luca.c.eftimie@gmail.com>
2024-09-07 21:00:48 +0300

    Prepared app for deployment

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const cookieParser = require('cookie-parser');
+const hpp = require('hpp');
 const compression = require('compression');
 //App Module - start txpress app
 const app = express();
